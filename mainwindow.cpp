cdae7be98b7e2049da0697d29ca42b6145afd3c9
Chad <chad@tangle.ca>
2017-01-24 16:59:46 -0700

    Getting started.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,13 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QtSql>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QSqlDatabase db = QSqlDatabase::addDatabase("QMYSQL");
 }
 
 MainWindow::~MainWindow()
